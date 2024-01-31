fd4bce09198c0ef5d84457a21d102b6efefc9d73
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2024-01-31 19:51:07 +0300

    getting started

---
+++ app/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+reportWebVitals();
