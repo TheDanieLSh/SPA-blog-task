ae70e8df7c6ef9922a677f7c7248ccaba4246a9d
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2024-02-05 14:00:00 +0300

    f5

--- app/src/components/Posts.tsx
+++ app/src/components/Posts.tsx
@@ -26,34 +26,40 @@ export default function Posts() {
     return (
         <main>
             {postsArray.length ? (
-                <div className="main__first-post">
-                    <img src="https://placehold.co/1140x600" />
-                    <div className="first-post__content">
-                        <div className="first-post__header">
-                            <div className="first-post__title">{postsArray[0].title}</div>
-                            <div className="first-post__reactions">
-                                <div className="reactions__likes">
-                                    <img src="like_grey.svg" />
-                                    <div className="count">0</div>
-                                </div>
-                                <div className="reactions__dislikes">
-                                    <img src="dislike_grey.svg" />
-                                    <div className="count">0</div>
+                <div className="main__posts">
+                    <div className="main__first-post">
+                        <img src="https://placehold.co/1140x600" />
+                        <div className="first-post__content">
+                            <div className="first-post__header">
+                                <div className="first-post__title">{postsArray[0].title}</div>
+                                <div className="first-post__reactions">
+                                    <div className="reactions__likes">
+                                        <img src="like_grey.svg" />
+                                        <div className="count">0</div>
+                                    </div>
+                                    <div className="reactions__dislikes">
+                                        <img src="dislike_grey.svg" />
+                                        <div className="count">0</div>
+                                    </div>
                                 </div>
                             </div>
+                            <div className="first-post__body">
+                                <p className="first-post__text">{postsArray[0].body}</p>
+                            </div>
+                            <div className="first-post__footer">
+                                <div className="first-post__button">Читать далее</div>
+                            </div>
                         </div>
-                        <div className="first-post__body">
-                            <p className="first-post__text">{postsArray[0].body}</p>
-                        </div>
-                        <div className="first-post__footer">
-                            <div className="first-post__button">Читать далее</div>
-                        </div>
+                    </div>
+                    <div className="main__further-posts">
+                        {postsArray.map((post, i) => (
+                            if (i  <= 3)
+                        ))}
                     </div>
                 </div>
             ) : (
                 <div>Posts not found.</div>
             )}
-            <div className="main__further-posts"></div>
         </main>
     )
 }
